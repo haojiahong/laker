ae15d0020b43c615ab39215e956e350c3947eea5
haojiahong <jiahong.hao@tqmall.com>
2017-02-13 19:34:03 +0800

    配置了autoconfig

--- laker.manager/src/test/java/com/hao/laker/biz/TestServiceTest.java
+++ laker.manager/src/test/java/com/hao/laker/biz/TestServiceTest.java
@@ -1,58 +1,52 @@
 package com.hao.laker.biz;
 
-import com.google.common.collect.Lists;
-import com.hao.laker.biz.test.TestService;
 import com.hao.laker.entity.test.Test1;
-import com.hao.laker.study.thread.MainProcessor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:test-biz-context.xml")
 //@Transactional
 @Slf4j
 public class TestServiceTest {
-    @Autowired
-    private TestService testService;
-    @Autowired
-    private MainProcessor mainProcessor;
-
-    @Test
-//    @Rollback(false)
-    public void demoTest() {
-        Test1 testOne = new Test1();
-        testOne.setTestName("testOne");
-        Test1 testTwo = new Test1();
-        testTwo.setTestName("test");
-        Test1 testThree = new Test1();
-        testThree.setTestName("testThree");
-        List<Test1> test1List = Lists.newArrayList();
-        test1List.add(testOne);
-        test1List.add(testTwo);
-        test1List.add(testThree);
-        int result = testService.batchInsertTest(test1List);
-        System.out.println(result);
-
-    }
-
+    //    @Autowired
+//    private TestService testService;
+//    @Autowired
+//    private MainProcessor mainProcessor;
+//
+//    @Test
+////    @Rollback(false)
+//    public void demoTest() {
+//        Test1 testOne = new Test1();
+//        testOne.setTestName("testOne");
+//        Test1 testTwo = new Test1();
+//        testTwo.setTestName("test");
+//        Test1 testThree = new Test1();
+//        testThree.setTestName("testThree");
+//        List<Test1> test1List = Lists.newArrayList();
+//        test1List.add(testOne);
+//        test1List.add(testTwo);
+//        test1List.add(testThree);
+//        int result = testService.batchInsertTest(test1List);
+//        System.out.println(result);
+//
+//    }
+//
     @Test
     public void demoTest2() {
         Test1 test1 = new Test1();
         test1.setTestName("test11111");
-        int result = testService.insertTest(test1);
-        System.out.println(result);
+//        int result = testService.insertTest(test1);
+        System.out.println(test1);
     }
-
-    @Test
-    public void threadTest() {
-        mainProcessor.testSpringTransactionThread();
-    }
+//
+//    @Test
+//    public void threadTest() {
+//        mainProcessor.testSpringTransactionThread();
+//    }
 
 }
 
